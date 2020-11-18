e28779a7d61f901081492c4aa624026ebbd8f993
Agam Adhityo <adhityoagam@gmail.com>
2020-11-18 18:32:49 +0700

    :airplane: header cacheable

--- routes/api.php
+++ routes/api.php
@@ -19,22 +19,24 @@ use App\Api\V1\Controllers\CategoryController;
 |
 */
 
-Route::post('/auth/login', [AuthController::class, 'login']);
+Route::middleware('cache.headers:public;max_age=3600;etag')->group(function () {
+    Route::post('/auth/login', [AuthController::class, 'login']);
 
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::post('/categories', [CategoryController::class, 'store'])->middleware('api.auth');
-Route::get('/categories/{category}', [CategoryController::class, 'show'])->middleware('api.auth');
-Route::patch('/categories/{category}', [CategoryController::class, 'update'])->middleware('api.auth');
-Route::delete('/categories/{category}', [CategoryController::class, 'delete'])->middleware('api.auth');
+    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::post('/categories', [CategoryController::class, 'store'])->middleware('api.auth');
+    Route::get('/categories/{category}', [CategoryController::class, 'show'])->middleware('api.auth');
+    Route::patch('/categories/{category}', [CategoryController::class, 'update'])->middleware('api.auth');
+    Route::delete('/categories/{category}', [CategoryController::class, 'delete'])->middleware('api.auth');
 
-Route::post('/media', [MediaController::class, 'store'])->middleware('api.auth');
+    Route::post('/media', [MediaController::class, 'store'])->middleware('api.auth');
 
-Route::get('/orders', [OrderController::class, 'index'])->middleware('api.auth');
-Route::post('/orders', [OrderController::class, 'store']);
-Route::get('/orders/{order}', [OrderController::class, 'show']);
+    Route::get('/orders', [OrderController::class, 'index'])->middleware('api.auth');
+    Route::post('/orders', [OrderController::class, 'store']);
+    Route::get('/orders/{order}', [OrderController::class, 'show']);
 
-Route::get('/products', [ProductController::class, 'index']);
-Route::post('/products', [ProductController::class, 'store'])->middleware('api.auth');
-Route::get('/products/{product}', [ProductController::class, 'show']);
-Route::patch('/products/{product}', [ProductController::class, 'update'])->middleware('api.auth');
-Route::delete('/products/{product}', [ProductController::class, 'delete'])->middleware('api.auth');
+    Route::get('/products', [ProductController::class, 'index']);
+    Route::post('/products', [ProductController::class, 'store'])->middleware('api.auth');
+    Route::get('/products/{product}', [ProductController::class, 'show']);
+    Route::patch('/products/{product}', [ProductController::class, 'update'])->middleware('api.auth');
+    Route::delete('/products/{product}', [ProductController::class, 'delete'])->middleware('api.auth');
+});
